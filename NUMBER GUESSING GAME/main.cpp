0f2fb4de8f552f7c1868e3d2b814c42cd6b8a413
jibrankhalil-jk <dev.jibran.khalil@gmail.com>
2024-04-23 23:43:55 +0500

    task 1 done

--- NUMBER GUESSING GAME/main.cpp
+++ NUMBER GUESSING GAME/main.cpp
@@ -3,32 +3,60 @@
 
 using namespace std;
 
-inline int generateRandomNumber()
+inline int generateRandomNumber(int min, int max)
 {
     srand(time(NULL));
-    int random = (rand() % 12) + 10;
+    int random = min + rand() % (max - min + 1);
     return random;
 };
 
 int main()
 {
-
-    int randomNumber = generateRandomNumber();
-    int guess;
-    cout << "Guess " << randomNumber << " \n";
-    int totalGuesses = 0;
-
+    int guess, min, max, totalGuesses = 0;
+    cout << "Enter range for a random number : \n";
     do
     {
-        cout << "Guess the Number : ";
+        cout<<"Enter Minimum : ";
+
+        cin >> min;
+        if (min >= 0)
+        {
+            break;
+        }
+        cout << "Minimum must ba a positive number !" << endl;
+    } while (1);
+    do
+    {
+        cout<<"Enter Maximum : ";
+        cin >> max;
+        if (max >= 0)
+        {
+            if (max <= min )
+            {
+                cout << "Maximum must b 1 digit greater than Minimum" << endl;
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        cout << "Maximum must ba a positive number !" << endl;
+
+    } while (1);
+    int randomNumber = generateRandomNumber(min, max);
+    cout<<"Lets check how long it will take you the guess the right number ! ."<<endl;
+    do
+    {
+        // cout << randomNumber << " Guess a Number : ";
+        cout <<  endl<<" Guess a Number : ";
         cin >> guess;
         ++totalGuesses;
-
         if (guess == randomNumber)
         {
             break;
         }
-
         if (guess > randomNumber)
         {
             if (guess < (randomNumber + 5))
@@ -55,9 +83,10 @@ int main()
 
     } while (guess != randomNumber);
 
-    cout << "Winner !!!!" << endl;
-
-    cout << "Total Guess : "<< totalGuesses << endl;
+    cout << "******************************************" << endl;
+    cout << "\t\t Winner !!!!" << endl;
+    cout << "\t\t Total Guess : " << totalGuesses << endl;
+    cout << "******************************************" << endl;
 
     return 0;
 }
